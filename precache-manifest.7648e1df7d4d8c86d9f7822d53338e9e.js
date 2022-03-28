c097f870ef7f7e80351b09c109163445225fd462
raqueleucaria <raqueleucaria@gmail.com>
2022-03-28 10:47:29 -0300

    Initial commit

---
+++ precache-manifest.7648e1df7d4d8c86d9f7822d53338e9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a89cf158e2673f44c929",
+    "url": "/static/css/main.07741d9d.chunk.css"
+  },
+  {
+    "revision": "a89cf158e2673f44c929",
+    "url": "/static/js/main.a89cf158.chunk.js"
+  },
+  {
+    "revision": "cec0d65ff439c1099d67",
+    "url": "/static/js/1.cec0d65f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86155d8fb3027c35270cb0b0ef1a64f6",
+    "url": "/index.html"
+  }
+];
